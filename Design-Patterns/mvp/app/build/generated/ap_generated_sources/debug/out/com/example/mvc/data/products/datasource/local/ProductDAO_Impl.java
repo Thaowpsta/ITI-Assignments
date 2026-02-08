a1b2e7c09535ec781126e712e3dea8f6748447f9
Thaowpsta <thaowpstasaiid@gmail.com>
2026-02-08 19:45:40 +0200

    Add generated build artifacts, update .gitignore

---
+++ Design-Patterns/mvp/app/build/generated/ap_generated_sources/debug/out/com/example/mvc/data/products/datasource/local/ProductDAO_Impl.java
@@ -0,0 +1,173 @@
+package com.example.mvc.data.products.datasource.local;
+
+import android.database.Cursor;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.rxjava3.RxRoom;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import com.example.mvc.data.products.model.Product;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Flowable;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.lang.Void;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import javax.annotation.processing.Generated;
+
+@Generated("androidx.room.RoomProcessor")
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class ProductDAO_Impl implements ProductDAO {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<Product> __insertionAdapterOfProduct;
+
+  private final EntityDeletionOrUpdateAdapter<Product> __deletionAdapterOfProduct;
+
+  public ProductDAO_Impl(@NonNull final RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfProduct = new EntityInsertionAdapter<Product>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "INSERT OR IGNORE INTO `fav_products` (`id`,`title`,`imgUrl`) VALUES (?,?,?)";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement, final Product entity) {
+        if (entity.getId() == null) {
+          statement.bindNull(1);
+        } else {
+          statement.bindString(1, entity.getId());
+        }
+        if (entity.getTitle() == null) {
+          statement.bindNull(2);
+        } else {
+          statement.bindString(2, entity.getTitle());
+        }
+        if (entity.getImgUrl() == null) {
+          statement.bindNull(3);
+        } else {
+          statement.bindString(3, entity.getImgUrl());
+        }
+      }
+    };
+    this.__deletionAdapterOfProduct = new EntityDeletionOrUpdateAdapter<Product>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "DELETE FROM `fav_products` WHERE `id` = ?";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement, final Product entity) {
+        if (entity.getId() == null) {
+          statement.bindNull(1);
+        } else {
+          statement.bindString(1, entity.getId());
+        }
+      }
+    };
+  }
+
+  @Override
+  public Completable insertProduct(final Product product) {
+    return Completable.fromCallable(new Callable<Void>() {
+      @Override
+      @Nullable
+      public Void call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfProduct.insert(product);
+          __db.setTransactionSuccessful();
+          return null;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    });
+  }
+
+  @Override
+  public Completable deleteProduct(final Product product) {
+    return Completable.fromCallable(new Callable<Void>() {
+      @Override
+      @Nullable
+      public Void call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __deletionAdapterOfProduct.handle(product);
+          __db.setTransactionSuccessful();
+          return null;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    });
+  }
+
+  @Override
+  public Flowable<List<Product>> getFavoriteProducts() {
+    final String _sql = "SELECT * FROM fav_products";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return RxRoom.createFlowable(__db, false, new String[] {"fav_products"}, new Callable<List<Product>>() {
+      @Override
+      public List<Product> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfTitle = CursorUtil.getColumnIndexOrThrow(_cursor, "title");
+          final int _cursorIndexOfImgUrl = CursorUtil.getColumnIndexOrThrow(_cursor, "imgUrl");
+          final List<Product> _result = new ArrayList<Product>(_cursor.getCount());
+          while (_cursor.moveToNext()) {
+            final Product _item;
+            final String _tmpId;
+            if (_cursor.isNull(_cursorIndexOfId)) {
+              _tmpId = null;
+            } else {
+              _tmpId = _cursor.getString(_cursorIndexOfId);
+            }
+            final String _tmpTitle;
+            if (_cursor.isNull(_cursorIndexOfTitle)) {
+              _tmpTitle = null;
+            } else {
+              _tmpTitle = _cursor.getString(_cursorIndexOfTitle);
+            }
+            final String _tmpImgUrl;
+            if (_cursor.isNull(_cursorIndexOfImgUrl)) {
+              _tmpImgUrl = null;
+            } else {
+              _tmpImgUrl = _cursor.getString(_cursorIndexOfImgUrl);
+            }
+            _item = new Product(_tmpId,_tmpTitle,_tmpImgUrl);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @NonNull
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
